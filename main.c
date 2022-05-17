cacbdcf3269748ddd0c50d399253d9ebfee8f95e
jeffdebruh <105702766+jeffdebruh@users.noreply.github.com>
2022-05-17 10:01:05 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+typedef struct{
+	int orientation:
+	int nbr_piece;
+	int piece[4][4];
+}piece;
+
+
+piece createpiece(){
+	piece x;
+	int 
+	srand(time(NULL));
+	x.nbr_piece=rand()%7;
+	switch (x.nbr_piece){
+		case 0:
+		
+			
+	scanf("%d",&x.orientation);
+
+
+
+
+
+
+
+int main(){
+	char board[10][10];
+
