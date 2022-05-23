af40f53467d3515602992ac7d0dc04e063c74582
jeffdebruh <105702766+jeffdebruh@users.noreply.github.com>
2022-05-23 16:00:33 +0200

    Update main.c

--- main.c
+++ main.c
@@ -22,12 +22,25 @@ piece createpiece(){
 			
 	scanf("%d",&x.orientation);
 
+int linefull(int y, char board){
+	int c=0;
+	for (i=0;i>9;i++){
+		if (board[i][y]=="@"){
+			c+=1;
+		}
+	}
+	return c;	
 
-
-
+int lineclear(int x,char board){
+	for (i=9;i>=0;i--){
+		if (linefull(i,board)==10){
+			lineclear(
+		}
+		
+	
 
 
 
 int main(){
 	char board[10][10];
-
+	
