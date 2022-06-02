01dfe412ed5d2ebae65071473ea5828f0df6afa6
jeffdebruh <105702766+jeffdebruh@users.noreply.github.com>
2022-06-02 17:35:54 +0200

    Update piece.c

--- piece.c
+++ piece.c
@@ -3,11 +3,10 @@
 #include <time.h>
 #include <string.h>
 
-
 typedef struct {
 	int orientation;
 	int piecenum;
-	char piecet[4][4];
+	int piecet[4][4];
 	 
 } piece;
 
@@ -18,172 +17,172 @@ piece createpiece(){
 	x.orientation = rand()%4;
 	memset(x.piecet, ' ', 16);
 	if (x.piecenum == 1 && x.orientation == 1){ // O
-		x.piecet[0][0] = '@';
-		x.piecet[0][1] = '@';
-		x.piecet[1][0] = '@';
-		x.piecet[1][1] = '@';
+		x.piecet[0][0] = 1;
+		x.piecet[0][1] = 1;
+		x.piecet[1][0] = 1;
+		x.piecet[1][1] = 1;
 	}
 	if (x.piecenum == 1 && x.orientation == 2){
-		x.piecet[0][0] = '@';
-		x.piecet[0][1] = '@';
-		x.piecet[1][0] = '@';
-		x.piecet[1][1] = '@';
+		x.piecet[0][0] = 1;
+		x.piecet[0][1] = 1;
+		x.piecet[1][0] = 1;
+		x.piecet[1][1] = 1;
 	}	
 	if (x.piecenum == 1 && x.orientation == 3){
-		x.piecet[0][0] = '@';
-		x.piecet[0][1] = '@';
-		x.piecet[1][0] = '@';
-		x.piecet[1][1] = '@';	
+		x.piecet[0][0] = 1;
+		x.piecet[0][1] = 1;
+		x.piecet[1][0] = 1;
+		x.piecet[1][1] = 1;	
 	}
 	if (x.piecenum == 1 && x.orientation == 4){
-		x.piecet[0][0] = '@';
-		x.piecet[0][1] = '@';
-		x.piecet[1][0] = '@';
-		x.piecet[1][1] = '@';
+		x.piecet[0][0] = 1;
+		x.piecet[0][1] = 1;
+		x.piecet[1][0] = 1;
+		x.piecet[1][1] = 1;
 	}
 	if (x.piecenum == 2 && x.orientation == 1){ // I
-		x.piecet[0][0] = '@';
-		x.piecet[0][1] = '@';
-		x.piecet[0][2] = '@';
-		x.piecet[0][3] = '@';	
+		x.piecet[0][0] = 1;
+		x.piecet[0][1] = 1;
+		x.piecet[0][2] = 1;
+		x.piecet[0][3] = 1;	
 	}
 	if (x.piecenum == 2 && x.orientation == 2){
-		x.piecet[0][0] = '@';
-		x.piecet[1][0] = '@';
-		x.piecet[2][0] = '@';
-		x.piecet[3][0] = '@';
+		x.piecet[0][0] = 1;
+		x.piecet[1][0] = 1;
+		x.piecet[2][0] = 1;
+		x.piecet[3][0] = 1;
 	}	
 	if (x.piecenum == 2 && x.orientation == 3){
-		x.piecet[0][0] = '@';
-		x.piecet[0][1] = '@';
-		x.piecet[0][2] = '@';
-		x.piecet[0][3] = '@';
+		x.piecet[0][0] = 1;
+		x.piecet[0][1] = 1;
+		x.piecet[0][2] = 1;
+		x.piecet[0][3] = 1;
 	}	
 	if (x.piecenum == 2 && x.orientation == 4){
-		x.piecet[0][0] = '@';
-		x.piecet[1][0] = '@';
-		x.piecet[2][0] = '@';
-		x.piecet[3][0] = '@';
+		x.piecet[0][0] = 1;
+		x.piecet[1][0] = 1;
+		x.piecet[2][0] = 1;
+		x.piecet[3][0] = 1;
 	}
 	if (x.piecenum == 3 && x.orientation == 1){ // S
-		x.piecet[0][3] = '@';
-		x.piecet[1][3] = '@';
-		x.piecet[2][2] = '@';
-		x.piecet[3][2] = '@';
+		x.piecet[0][3] = 1;
+		x.piecet[1][3] = 1;
+		x.piecet[2][2] = 1;
+		x.piecet[3][2] = 1;
 	}
 	if (x.piecenum == 3 && x.orientation == 2){
-		x.piecet[1][3] = '@';
-		x.piecet[1][2] = '@';
-		x.piecet[0][2] = '@';
-		x.piecet[0][1] = '@';
+		x.piecet[1][3] = 1;
+		x.piecet[1][2] = 1;
+		x.piecet[0][2] = 1;
+		x.piecet[0][1] = 1;
 	}
 	if (x.piecenum == 3 && x.orientation == 3){
-		x.piecet[0][3] = '@';
-		x.piecet[1][3] = '@';
-		x.piecet[2][2] = '@';
-		x.piecet[3][2] = '@';
+		x.piecet[0][3] = 1;
+		x.piecet[1][3] = 1;
+		x.piecet[2][2] = 1;
+		x.piecet[3][2] = 1;
 	}
 	if (x.piecenum == 3 && x.orientation == 4){
-		x.piecet[1][3] = '@';
-		x.piecet[1][2] = '@';
-		x.piecet[0][2] = '@';
-		x.piecet[0][1] = '@';
+		x.piecet[1][3] = 1;
+		x.piecet[1][2] = 1;
+		x.piecet[0][2] = 1;
+		x.piecet[0][1] = 1;
 	}
 	if (x.piecenum == 4 && x.orientation == 1){ // Z
-		x.piecet[2][3] = '@';
-		x.piecet[1][3] = '@';
-		x.piecet[1][2] = '@';
-		x.piecet[0][2] = '@';
+		x.piecet[2][3] = 1;
+		x.piecet[1][3] = 1;
+		x.piecet[1][2] = 1;
+		x.piecet[0][2] = 1;
 	}
 	if (x.piecenum == 4 && x.orientation == 2){
-		x.piecet[0][3] = '@';
-		x.piecet[0][2] = '@';
-		x.piecet[1][2] = '@';
-		x.piecet[1][1] = '@';
+		x.piecet[0][3] = 1;
+		x.piecet[0][2] = 1;
+		x.piecet[1][2] = 1;
+		x.piecet[1][1] = 1;
 	}
 	if (x.piecenum == 4 && x.orientation == 3){
-		x.piecet[2][3] = '@';
-		x.piecet[1][3] = '@';
-		x.piecet[1][2] = '@';
-		x.piecet[0][2] = '@';
+		x.piecet[2][3] = 1;
+		x.piecet[1][3] = 1;
+		x.piecet[1][2] = 1;
+		x.piecet[0][2] = 1;
 	}
 	if (x.piecenum == 4 && x.orientation == 4){
-		x.piecet[0][3] = '@';
-		x.piecet[0][2] = '@';
-		x.piecet[1][2] = '@';
-		x.piecet[1][1] = '@';
+		x.piecet[0][3] = 1;
+		x.piecet[0][2] = 1;
+		x.piecet[1][2] = 1;
+		x.piecet[1][1] = 1;
 	}
 	if (x.piecenum == 5 && x.orientation == 1){ // L
-		x.piecet[0][1] = '@';
-		x.piecet[0][2] = '@';
-		x.piecet[0][3] = '@';
-		x.piecet[1][3] = '@';
+		x.piecet[0][1] = 1;
+		x.piecet[0][2] = 1;
+		x.piecet[0][3] = 1;
+		x.piecet[1][3] = 1;
 	}
 	if (x.piecenum == 5 && x.orientation == 2){
-		x.piecet[0][3] = '@';
-		x.piecet[0][2] = '@';
-		x.piecet[1][2] = '@';
-		x.piecet[2][2] = '@';
+		x.piecet[0][3] = 1;
+		x.piecet[0][2] = 1;
+		x.piecet[1][2] = 1;
+		x.piecet[2][2] = 1;
 	}
 	if (x.piecenum == 5 && x.orientation == 3){
-		x.piecet[1][3] = '@';
-		x.piecet[1][2] = '@';
-		x.piecet[1][1] = '@';
-		x.piecet[0][1] = '@';
+		x.piecet[1][3] = 1;
+		x.piecet[1][2] = 1;
+		x.piecet[1][1] = 1;
+		x.piecet[0][1] = 1;
 	}
 	if (x.piecenum == 5 && x.orientation == 4){
-		x.piecet[0][3] = '@';
-		x.piecet[1][3] = '@';
-		x.piecet[2][3] = '@';
-		x.piecet[2][2] = '@';
+		x.piecet[0][3] = 1;
+		x.piecet[1][3] = 1;
+		x.piecet[2][3] = 1;
+		x.piecet[2][2] = 1;
 	}
 	if (x.piecenum == 6 && x.orientation == 1){ // J
-		x.piecet[0][3] = '@';
-		x.piecet[1][3] = '@';
-		x.piecet[1][2] = '@';
-		x.piecet[1][1] = '@';	
+		x.piecet[0][3] = 1;
+		x.piecet[1][3] = 1;
+		x.piecet[1][2] = 1;
+		x.piecet[1][1] = 1;	
 	}
 	if (x.piecenum == 6 && x.orientation == 2){
-		x.piecet[0][2] = '@';
-		x.piecet[0][3] = '@';
-		x.piecet[1][3] = '@';
-		x.piecet[1][2] = '@';	
+		x.piecet[0][2] = 1;
+		x.piecet[0][3] = 1;
+		x.piecet[1][3] = 1;
+		x.piecet[1][2] = 1;	
 	}
 	if (x.piecenum == 6 && x.orientation == 3){
-		x.piecet[0][2] = '@';
-		x.piecet[0][3] = '@';
-		x.piecet[0][1] = '@';
-		x.piecet[1][1] = '@';	
+		x.piecet[0][2] = 1;
+		x.piecet[0][3] = 1;
+		x.piecet[0][1] = 1;
+		x.piecet[1][1] = 1;	
 	}
 	if (x.piecenum == 6 && x.orientation == 4){
-		x.piecet[1][3] = '@';
-		x.piecet[2][3] = '@';
-		x.piecet[3][3] = '@';
-		x.piecet[3][2] = '@';	
+		x.piecet[1][3] = 1;
+		x.piecet[2][3] = 1;
+		x.piecet[3][3] = 1;
+		x.piecet[3][2] = 1;	
 	}
 	if (x.piecenum == 7 && x.orientation == 1){ // T
-		x.piecet[1][3] = '@';
-		x.piecet[0][2] = '@';
-		x.piecet[1][2] = '@';
-		x.piecet[2][2] = '@';	
+		x.piecet[1][3] = 1;
+		x.piecet[0][2] = 1;
+		x.piecet[1][2] = 1;
+		x.piecet[2][2] = 1;	
 	}
 	if (x.piecenum == 7 && x.orientation == 2){
-		x.piecet[1][3] = '@';
-		x.piecet[1][2] = '@';
-		x.piecet[1][1] = '@';
-		x.piecet[0][2] = '@';	
+		x.piecet[1][3] = 1;
+		x.piecet[1][2] = 1;
+		x.piecet[1][1] = 1;
+		x.piecet[0][2] = 1;	
 	}
 	if (x.piecenum == 7 && x.orientation == 3){
-		x.piecet[0][3] = '@';
-		x.piecet[1][3] = '@';
-		x.piecet[2][3] = '@';
-		x.piecet[1][2] = '@';	
+		x.piecet[0][3] = 1;
+		x.piecet[1][3] = 1;
+		x.piecet[2][3] = 1;
+		x.piecet[1][2] = 1;	
 	}
 	if (x.piecenum == 7 && x.orientation == 4){	
-		x.piecet[0][3] = '@';
-		x.piecet[0][2] = '@';
-		x.piecet[0][1] = '@';
-		x.piecet[1][2] = '@';	
+		x.piecet[0][3] = 1;
+		x.piecet[0][2] = 1;
+		x.piecet[0][1] = 1;
+		x.piecet[1][2] = 1;	
 	}
 	return x;
 }
