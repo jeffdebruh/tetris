87d98adf7e8cd16a5fa4545640617db2e70485b6
jeffdebruh <105702766+jeffdebruh@users.noreply.github.com>
2022-06-05 01:27:14 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,10 +3,13 @@
 #include <string.h>
 #include <time.h>
 
-void printboard(int board[][10]) {
-  puts(" A B C D E F H I J");
-  for (int i = 0; i < 9; i++) {
-    for (int j = 0; j < 9; j++) {
+void printboard(int board[][10],int pts) {
+  puts("_____________________");
+  printf("score:%d",pts);
+  puts("");
+  puts(" A B C D E F G H I J");
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 10; j++) {
       switch (board[i][j]){
         case 1:
           printf("|@");
@@ -23,7 +26,7 @@ void printboard(int board[][10]) {
 }
 
 int linefull(int y, int board[][10]) {
-  for (int i = 0; i > 9; i++) {
+  for (int i = 0; i > 10; i++) {
     if (board[i][y]== 0) {
       return 0;
     }
@@ -31,56 +34,58 @@ int linefull(int y, int board[][10]) {
   return 1;
 }
 
-int awardpoints(int lines, int points, int dif) {
+int awardpts(int lines, int pts, int dif) {
   switch (lines) {
   case 1:
-    points += 40 * (dif + 1);
+    pts += 40 * (dif + 1);
     break;
   case 2:
-    points += 100 * (dif + 1);
+    pts += 100 * (dif + 1);
     break;
   case 3:
-    points += 300 * (dif + 1);
+    pts += 300 * (dif + 1);
     break;
   case 4:
-    points += 1200 * (dif + 1);
+    pts += 1200 * (dif + 1);
     break;
   default:
-    printf("error too much lines");
+    puts("error too much lines");
   }
-  return points;
+  return pts;
 }
 
-void fall(int board[][10]) {
-  for (int i = 8; i < 0; i--) {
-    for (int j = 0; j < 9; j++) {
-      if (board[i][j]==0 && board[i + 1][j]== 1) {
-        board[i][j] =1;
-        board[i+1][j] =0;
+void fall(int board[][10],int pts,int x) {
+    for (int i = x; i >= 0; i--) {
+      for (int j = 0; j < 10; j++) {
+        if (board[i][j]==1 && board[i+1][j]== 0) {
+          board[i][j] =0;
+          board[i+1][j] =1;
+        }
       }
     }
-  }
 }
 
-int lineclear(int x, int board[][10], int points, int dif) {
-  for (int i = 9; i >= 0; i--) {
+int lineclear(int board[][10], int pts, int dif) {
+  int x=0;
+  for (int i = 0; i < 10; i++) {
     if (linefull(i, board) == 1) {
-      for (int j = 0; j > 9; i++) {
-        board[i][j] = 0;
+      for (int j = 0; j > 10; i++) {
+        board[i][j]=0;
+        }
         x += 1;
-        return 0;
-      }
     }
-    awardpoints(x, points, dif);
   }
-  return 0;
+  x=awardpts(x, pts, dif);
+  return x;
 }
 
 int main() {
-  int points=0;
+  int pts=0;
   int dif = 0;
-  int board[10][10] = {{}};
-  board[7]={{0,0,0,0,0,0,0,0,0,0}};
-  printboard(board);
+  int x;
+  int board[10][10] = {[5]={1,1,1,1,1,1,1,1,1,1}};
+  printboard(board,pts);
+  fall(board,pts,4);
+  printboard(board,pts);
   return 0;
 }
